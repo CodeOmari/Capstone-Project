56eb6bb0b8db2599b4d3a8eccb02f6c952bc924d
CodeOmari <jacksonomari2230@gmail.com>
2025-03-25 15:09:30 +0300

    added register and login url configurations

--- events_management_api/api_app/urls.py
+++ events_management_api/api_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet
+from .views import EventViewSet, RegisterView, LoginView
 
 router = DefaultRouter()
 router.register(r'events', EventViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
