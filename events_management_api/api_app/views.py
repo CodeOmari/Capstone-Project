0482124eec07233d5fc0ae7c716cfe8f23fdfe0d
CodeOmari <jacksonomari2230@gmail.com>
2025-03-25 15:10:30 +0300

    added the user registration and login views

--- events_management_api/api_app/views.py
+++ events_management_api/api_app/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
-from .serializers import EventSerializer
+from .serializers import EventSerializer, UserProfileSerializer
 from .models import Event
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()  # Token creation handled in the serializer
+            return Response({"user": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            token = user.auth_token.key  # Token directly retrieved
+            user_data = UserProfileSerializer(user).data
+            return Response({"token": token, "user": user_data}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
