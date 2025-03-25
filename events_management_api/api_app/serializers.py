1465bf2997a785c1edc1718432c415c208fa9034
CodeOmari <jacksonomari2230@gmail.com>
2025-03-25 15:08:27 +0300

    added the user registration custom fields for validation

--- events_management_api/api_app/serializers.py
+++ events_management_api/api_app/serializers.py
@@ -1,6 +1,38 @@
 from rest_framework import serializers
-from .models import Event, Ticket
+from .models import Event, Ticket, UserProfile
 from datetime import date
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+
+UserProfile = get_user_model() # Dynamically get custom user when using a custom model
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    password = serializers.CharField() # password won't be exposed and will be write-only
+
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+    # Describes what will happen when a user registers
+    def create(self, validated_data):
+
+        
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            phone_number=validated_data.get('phone_number'),
+            role=validated_data.get('role'),
+        )
+        
+        # Ensure user is saved before creating a token
+        user.save()
+
+        # Check if a token already exists for this user
+        token, created = Token.objects.get_or_create(user=user)
+        
+        return user
 
 
 
