15e4e296a22a4f9bc15159ea032ff335ff49ddb8
CodeOmari <jacksonomari2230@gmail.com>
2025-03-25 12:27:08 +0300

    added validation for event date field

--- events_management_api/api_app/serializers.py
+++ events_management_api/api_app/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
 from .models import Event, Ticket
+from datetime import date
+
+
 
 class EventSerializer(serializers.ModelSerializer):
-    model = Event
-    fields = '__all__'
-    read_only_fields = ['id', 'created_at', 'update_at']
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at', 'update_at']
+
+
+    def validate_event_date(self, value):
+        if value < date.today():
+            raise serializers.ValidationError('Event date must be in the future')
+        return value
+
+
+
+
 
 
 class TicketSerializer(serializers.ModelSerializer):
-    model = Ticket
-    fields = '__all__'
-    read_only_fields = ['id', 'created_at']
+    class Meta:
+        model = Ticket
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at']
