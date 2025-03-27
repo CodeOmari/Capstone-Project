72592592ec2eb0878e14986f080d24ae6060bc7f
CodeOmari <jacksonomari2230@gmail.com>
2025-03-27 07:51:31 +0300

    updated the registration and login urls

--- events_management_api/api_app/urls.py
+++ events_management_api/api_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet, RegisterView, LoginView
+from .views import EventViewSet, AuthViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'events', EventViewSet)
@@ -8,6 +9,6 @@ router.register(r'events', EventViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('register/', views.AuthViewSet.as_view({'post': 'register'}), name='register'),
+    path('login/', views.AuthViewSet.as_view({'post': 'login'}), name='login'),
 ]
