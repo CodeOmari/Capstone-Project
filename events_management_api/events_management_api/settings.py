e2cb3c795ac793a92f2ca070d560af379a43accf
CodeOmari <jacksonomari2230@gmail.com>
2025-03-27 07:53:14 +0300

    Added the token authentication configuration settings

--- events_management_api/events_management_api/settings.py
+++ events_management_api/events_management_api/settings.py
@@ -127,4 +127,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-AUTH_USER_MODEL = 'api_app.UserProfile'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Session Authentication
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Ensure the user is authenticated
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, #number of items per page
+}
