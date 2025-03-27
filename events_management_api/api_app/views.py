b92b92e86aafcafe6fedf4f6a90d037fad7df1a9
CodeOmari <jacksonomari2230@gmail.com>
2025-03-27 07:52:17 +0300

    Updated the user registration and login viewset

--- events_management_api/api_app/views.py
+++ events_management_api/api_app/views.py
@@ -1,62 +1,45 @@
-from django.shortcuts import render
-from .serializers import EventSerializer, UserProfileSerializer
+from .serializers import EventSerializer
 from .models import Event
-from rest_framework import viewsets, permissions
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from django.contrib.auth import authenticate
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
+ 
 
 # Create your views here.
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = UserProfileSerializer(data=request.data)
+class AuthViewSet(viewsets.ViewSet):
+
+    # allow anyone to register or login
+    permission_classes = [AllowAny]
+    
+    def register(self, request):
+        serializer = UserSerializer(data=request.data)
+     
         if serializer.is_valid():
-            user = serializer.save()  # Token creation handled in the serializer
-            return Response({"user": serializer.data}, status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response({"message":"user created successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+          
+    def login(self, request):
+        username= request.data.get('username')
+        password= request.data.get('password')
 
         user = authenticate(username=username, password=password)
+
         if user:
-            token = user.auth_token.key  # Token directly retrieved
-            user_data = UserProfileSerializer(user).data
-            return Response({"token": token, "user": user_data}, status=status.HTTP_200_OK)
-        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-
-
-
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"message":"login successful", 'token':token.key}, status=status.HTTP_200_OK)
+        return Response({"error":"incorrect username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+    
 
 class EventViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-    # Ensures only users with Organizer role can create events
-    def perform_create(self, serializer):
-        if user.request.user.role != 'Organizer':
-            raise PermissionDenied("Only Organizers can create events.")
-        serializer.save(event_organizer=self.request.user)
-
-    # Ensures Organizers can only update their own events
-    def perform_update(self, serializer):
-        event = self.get_object()
-        if event.event_organizer != self.request.user:
-            raise PermissionDenied("You can only update your own events.")
-        serializer.save()
-
-    
-    def perform_destroy(self, instance):
-        if instance.event_organizer != self.request.user:
-            raise PermissionDenied("You can only delete your own events")
-        instance.delete()
+   
 
 
