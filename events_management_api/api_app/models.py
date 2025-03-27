29d3476b0fe4f5eddbafccb0b2af517af63ee049
CodeOmari <jacksonomari2230@gmail.com>
2025-03-27 07:50:10 +0300

    updated the Event and Ticket models

--- events_management_api/api_app/models.py
+++ events_management_api/api_app/models.py
@@ -1,45 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.conf import settings
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-
-class UserProfile(AbstractUser):
-    ROLE_CHOICES = [
-        ('Organizer', 'Organizer'),
-        ('Attendee', 'Attendee'),
-    ]
-
-    role = models.CharField(max_length=100, choices=ROLE_CHOICES, default='Attendee')
-    phone_number = models.CharField(max_length=15, blank=False, null=False, unique=True)
-
-
-    # Add unique related_name attributes to avoid conflicts
-    groups = models.ManyToManyField(
-        "auth.Group",
-        related_name="userprofile_set",  # Custom related_name to avoid clash
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        "auth.Permission",
-        related_name="userprofile_permissions_set",  # Custom related_name to avoid clash
-        blank=True
-    )
-
-
-    def __str__(self):
-        return f'{self.username} - {self.role}'
-
-    
-    class Meta:
-        db_table = 'Users'
-
-
-
 class Event(models.Model):
     CATEGORY_CHOICES = [
-        ('', ''), 
         ('Tech', 'Tech'),
         ('Business', 'Business'),
         ('Social', 'Social'),
@@ -48,6 +13,7 @@ class Event(models.Model):
         ('Education', 'Education'),
     ]
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
     event_title = models.CharField(max_length=100)
     event_category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
     event_description = models.TextField()
@@ -57,7 +23,7 @@ class Event(models.Model):
     virtual_location = models.URLField(blank=True, null=True, default='N/A')
     event_price = models.DecimalField(max_digits=6, decimal_places=2, default='Free')
     event_capacity = models.IntegerField()
-    event_organizer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='organizer_events')
+    event_organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizer_events')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -73,7 +39,8 @@ class Event(models.Model):
 
 class Ticket(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='event_ticket')
-    attendee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='event_attendee')
+    attendee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
+    phone_number = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
