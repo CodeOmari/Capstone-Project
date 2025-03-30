cd8c4c1d71e20f29d45f5b5dda934cd2ad53dd4f
CodeOmari <jacksonomari2230@gmail.com>
2025-03-30 08:35:35 +0300

    added registration viewset url

--- events_management_api/api_app/urls.py
+++ events_management_api/api_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet, AuthViewSet, UpcomingEventViewSet
+from .views import EventViewSet, AuthViewSet, UpcomingEventViewSet, RegistrationViewSet
 from . import views
 
 router = DefaultRouter()
 router.register(r'events', EventViewSet)
 router.register(r'upcoming-events', UpcomingEventViewSet, basename='upcoming-events')
+router.register(r'registrations', RegistrationViewSet, basename='registrations')
+# router.register(r'event-slots', EventSlotsViewSet, basename='event-slots')
 
 urlpatterns = [
     path('', include(router.urls)),
