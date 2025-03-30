f65ff52d9864e14a33b54a007b1eec14e08eb1f8
CodeOmari <jacksonomari2230@gmail.com>
2025-03-30 13:55:42 +0300

    added attendees url

--- events_management_api/api_app/urls.py
+++ events_management_api/api_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet, AuthViewSet, UpcomingEventViewSet, RegistrationViewSet
+from .views import EventViewSet, AuthViewSet, UpcomingEventViewSet, RegistrationViewSet, OrganizerAttendeesViewSet
 from . import views
 
 router = DefaultRouter()
 router.register(r'events', EventViewSet)
 router.register(r'upcoming-events', UpcomingEventViewSet, basename='upcoming-events')
 router.register(r'registrations', RegistrationViewSet, basename='registrations')
-# router.register(r'event-slots', EventSlotsViewSet, basename='event-slots')
+router.register(r'attendees', OrganizerAttendeesViewSet, basename='attendees')
 
 urlpatterns = [
     path('', include(router.urls)),
