01b6c3923810f53f0d9423404ad35e2e5f9941bc
CodeOmari <jacksonomari2230@gmail.com>
2025-09-25 15:44:27 +0300

    update user authentication urls to use jwt authentication

--- events_management_api/events_management_api/urls.py
+++ events_management_api/events_management_api/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from events_management_api import settings
+from api_app.views import UserCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('register/', UserCreateView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('api/', include('api_app.urls')), 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
