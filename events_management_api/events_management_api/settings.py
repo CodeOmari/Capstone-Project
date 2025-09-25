b8f9b56aa860967d6a403a236c792e0310549a2a
CodeOmari <jacksonomari2230@gmail.com>
2025-09-25 16:36:39 +0300

    add whitenoise middleware

--- events_management_api/events_management_api/settings.py
+++ events_management_api/events_management_api/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'events_management_api.urls'
